e6866655d2f450a7069eb2aa0733ed813ecd6884
Daniel Correa <yo@danielgara.com>
2021-11-20 15:30:38 -0500

    imp: Chapter07

--- Chapter07/onlineStore/resources/views/layouts/app.blade.php
+++ Chapter07/onlineStore/resources/views/layouts/app.blade.php
@@ -11,15 +11,15 @@
   <!-- header -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
     <div class="container">
-      <a class="navbar-brand" href="#">Online Store</a>
+      <a class="navbar-brand" href="{{ route('home.index') }}">Online Store</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
         aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav ms-auto">
-          <a class="nav-link active" href="#">Home</a>
-          <a class="nav-link active" href="#">About</a>
+          <a class="nav-link active" href="{{ route('home.index') }}">Home</a>
+          <a class="nav-link active" href="{{ route('home.about') }}">About</a>
         </div>
       </div>
     </div>
--- Chapter07/onlineStore/routes/web.php
+++ Chapter07/onlineStore/routes/web.php
@@ -13,9 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $viewData = [];
-    $viewData["title"] = "Home Page - Online Store";
-    return view('home.index')->with("viewData", $viewData);
-}); 
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
 Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
