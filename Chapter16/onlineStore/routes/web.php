36873b95e8067516b489f445720b67bc6d0ee2ea
Daniel Correa <yo@danielgara.com>
2021-11-21 10:38:38 -0500

    imp: Chapter16

--- Chapter16/onlineStore/routes/web.php
+++ Chapter16/onlineStore/routes/web.php
@@ -18,3 +18,4 @@ Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.ab
 Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
 Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name("admin.home.index");
+Route::get('/admin/products', 'App\Http\Controllers\Admin\AdminProductController@index')->name("admin.product.index");
