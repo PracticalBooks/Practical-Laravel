1e0a1fddcb33795ff5611562952c11679dc299a8
Daniel Correa <yo@danielgara.com>
2021-11-21 10:39:30 -0500

    imp: pre-chapter17

---
+++ Chapter17/onlineStore/app/Http/Controllers/Admin/AdminProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Product;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminProductController extends Controller
+{
+    public function index()
+    {
+        $viewData = [];
+        $viewData["title"] = "Admin Page - Products - Online Store";
+        $viewData["products"] = Product::all();
+        return view('admin.product.index')->with("viewData", $viewData);
+    }
+}
