b675851e1205fac6897793fb451e608479f3b030
Daniel Correa <yo@danielgara.com>
2021-11-21 10:47:56 -0500

    imp: Chapter17

--- Chapter17/onlineStore/app/Http/Controllers/Admin/AdminProductController.php
+++ Chapter17/onlineStore/app/Http/Controllers/Admin/AdminProductController.php
@@ -15,4 +15,23 @@ class AdminProductController extends Controller
         $viewData["products"] = Product::all();
         return view('admin.product.index')->with("viewData", $viewData);
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            "name" => "required|max:255",
+            "description" => "required",
+            "price" => "required|numeric|gt:0",
+            'image' => 'image',
+        ]);
+
+        $newProduct = new Product();
+        $newProduct->setName($request->input('name'));
+        $newProduct->setDescription($request->input('description'));
+        $newProduct->setPrice($request->input('price'));
+        $newProduct->setImage("game.png");
+        $newProduct->save();
+
+        return back();
+    }
 }
--- Chapter17/onlineStore/routes/web.php
+++ Chapter17/onlineStore/routes/web.php
@@ -19,3 +19,4 @@ Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("p
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
 Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name("admin.home.index");
 Route::get('/admin/products', 'App\Http\Controllers\Admin\AdminProductController@index')->name("admin.product.index");
+Route::post('/admin/products/store', 'App\Http\Controllers\Admin\AdminProductController@store')->name("admin.product.store");
