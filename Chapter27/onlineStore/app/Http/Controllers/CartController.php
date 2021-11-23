62d65f8e9521a114ba80939dd26e3593dd0ca6ab
Daniel Correa <yo@danielgara.com>
2021-11-23 11:19:19 -0500

    imp: Chapter27

--- Chapter27/onlineStore/app/Http/Controllers/CartController.php
+++ Chapter27/onlineStore/app/Http/Controllers/CartController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Order;
+use App\Models\Item;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
@@ -39,5 +42,46 @@ class CartController extends Controller
     {
         $request->session()->forget('products');
         return back();
+    }
+
+    public function purchase(Request $request)
+    {
+        $productsInSession = $request->session()->get("products");
+        if ($productsInSession) {
+            $userId = Auth::user()->getId();
+            $order = new Order();
+            $order->setUserId($userId);
+            $order->setTotal(0);
+            $order->save();
+
+            $total = 0;
+            $productsInCart = Product::findMany(array_keys($productsInSession));
+            foreach ($productsInCart as $product) {
+                $quantity = $productsInSession[$product->getId()];
+                $item = new Item();
+                $item->setQuantity($quantity);
+                $item->setPrice($product->getPrice());
+                $item->setProductId($product->getId());
+                $item->setOrderId($order->getId());
+                $item->save();
+                $total = $total + ($product->getPrice()*$quantity);
+            }
+            $order->setTotal($total);
+            $order->save();
+
+            $newBalance = Auth::user()->getBalance() - $total;
+            Auth::user()->setBalance($newBalance);
+            Auth::user()->save();
+
+            $request->session()->forget('products');
+
+            $viewData = [];
+            $viewData["title"] = "Purchase - Online Store";
+            $viewData["subtitle"] =  "Purchase Status";
+            $viewData["order"] =  $order;
+            return view('cart.purchase')->with("viewData", $viewData);
+        } else {
+            return redirect()->route('cart.index');
+        }
     }
 }
