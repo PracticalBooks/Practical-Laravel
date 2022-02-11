19dca1b15c77fd3d7bf1da5894c1fbc4079844a1
Daniel Correa <yo@danielgara.com>
2022-02-11 12:28:31 -0500

    imp: Chapter14

--- Chapter14/onlineStore/app/Http/Controllers/ProductController.php
+++ Chapter14/onlineStore/app/Http/Controllers/ProductController.php
@@ -21,8 +21,8 @@ class ProductController extends Controller
     {
         $viewData = [];
         $product = Product::findOrFail($id);
-        $viewData["title"] = $product["name"]." - Online Store";
-        $viewData["subtitle"] =  $product["name"]." - Product information";
+        $viewData["title"] = $product->getName()." - Online Store";
+        $viewData["subtitle"] =  $product->getName()." - Product information";
         $viewData["product"] = $product;
         return view('product.show')->with("viewData", $viewData);
     }
--- Chapter14/onlineStore/resources/views/product/index.blade.php
+++ Chapter14/onlineStore/resources/views/product/index.blade.php
@@ -6,10 +6,10 @@
   @foreach ($viewData["products"] as $product)
   <div class="col-md-4 col-lg-3 mb-2">
     <div class="card">
-      <img src="{{ asset('/img/'.$product["image"]) }}" class="card-img-top img-card">
+      <img src="{{ asset('/img/'.$product->getImage()) }}" class="card-img-top img-card">
       <div class="card-body text-center">
-        <a href="{{ route('product.show', ['id'=> $product["id"]]) }}"
-          class="btn bg-primary text-white">{{ $product["name"] }}</a>
+        <a href="{{ route('product.show', ['id'=> $product->getId()]) }}"
+          class="btn bg-primary text-white">{{ $product->getName() }}</a>
       </div>
     </div>
   </div>
