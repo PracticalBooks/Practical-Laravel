3adb31f8ef11137a1709a00ad987bea44a37fcb6
Daniel Correa <yo@danielgara.com>
2022-02-11 09:28:55 -0500

    imp: Chapter07

--- Chapter07/onlineStore/app/Http/Controllers/HomeController.php
+++ Chapter07/onlineStore/app/Http/Controllers/HomeController.php
@@ -14,14 +14,12 @@ class HomeController extends Controller
 
   public function about()
   {
-    $data1 = "About us - Online Store";
-    $data2 = "About us";
-    $description = "This is an about page ...";
-    $author = "Developed by: Your Name";
-    return view('home.about')->with("title", $data1)
-      ->with("subtitle", $data2)
-      ->with("description", $description)
-      ->with("author", $author);
-
+    $viewData = [];
+    $viewData["title"] = "About us - Online Store";
+    $viewData["subtitle"] =  "About us";
+    $viewData["description"] =  "This is an about page ...";
+    $viewData["author"] = "Developed by: Your Name";
+    return view('home.about')->with("viewData", $viewData);
+    
   }
 }
--- Chapter07/onlineStore/resources/views/home/about.blade.php
+++ Chapter07/onlineStore/resources/views/home/about.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
-@section('title', $title)
-@section('subtitle', $subtitle)
+@section('title', $viewData["title"])
+@section('subtitle', $viewData["subtitle"])
 @section('content')
 <div class="container">
   <div class="row">
     <div class="col-lg-4 ms-auto">
-      <p class="lead">{{ $description }}</p>
+      <p class="lead">{{ $viewData["description"] }}</p>
     </div>
     <div class="col-lg-4 me-auto">
-      <p class="lead">{{ $author }}</p>
+      <p class="lead">{{ $viewData["author"] }}</p>
     </div>
   </div>
 </div>
