df70eb8b38de046fac4a731dd6a5cd55c3d9886a
Daniel Correa <yo@danielgara.com>
2022-02-12 09:02:50 -0500

    imp: Chapter21

--- Chapter21/onlineStore/resources/views/layouts/app.blade.php
+++ Chapter21/onlineStore/resources/views/layouts/app.blade.php
@@ -21,6 +21,17 @@
           <a class="nav-link active" href="{{ route('home.index') }}">Home</a>
           <a class="nav-link active" href="{{ route('product.index') }}">Products</a>
           <a class="nav-link active" href="{{ route('home.about') }}">About</a>
+          <div class="vr bg-white mx-2 d-none d-lg-block"></div>
+          @guest
+          <a class="nav-link active" href="{{ route('login') }}">Login</a>
+          <a class="nav-link active" href="{{ route('register') }}">Register</a>
+          @else
+          <form id="logout" action="{{ route('logout') }}" method="POST">
+            <a role="button" class="nav-link active"
+               onclick="document.getElementById('logout').submit();">Logout</a>
+            @csrf
+          </form>
+          @endguest
         </div>
       </div>
     </div>
--- Chapter21/onlineStore/routes/web.php
+++ Chapter21/onlineStore/routes/web.php
@@ -23,3 +23,5 @@ Route::post('/admin/products/store', 'App\Http\Controllers\Admin\AdminProductCon
 Route::delete('/admin/products/{id}/delete', 'App\Http\Controllers\Admin\AdminProductController@delete')->name("admin.product.delete");
 Route::get('/admin/products/{id}/edit', 'App\Http\Controllers\Admin\AdminProductController@edit')->name("admin.product.edit");
 Route::put('/admin/products/{id}/update', 'App\Http\Controllers\Admin\AdminProductController@update')->name("admin.product.update");
+
+Auth::routes();
