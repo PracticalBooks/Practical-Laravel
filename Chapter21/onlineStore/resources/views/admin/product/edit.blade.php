ba7e12fe8c2822df0f2a2be95744ecfc1aa3987c
Daniel Correa <yo@danielgara.com>
2022-02-12 08:30:54 -0500

    imp: pre-chapter21

---
+++ Chapter21/onlineStore/resources/views/admin/product/edit.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.admin')
+@section('title', $viewData["title"])
+@section('content')
+<div class="card mb-4">
+  <div class="card-header">
+    Edit Product
+  </div>
+  <div class="card-body">
+    @if($errors->any())
+    <ul class="alert alert-danger list-unstyled">
+      @foreach($errors->all() as $error)
+      <li>- {{ $error }}</li>
+      @endforeach
+    </ul>
+    @endif
+
+    <form method="POST" action="{{ route('admin.product.update', ['id'=> $viewData['product']->getId()]) }}"
+      enctype="multipart/form-data">
+      @csrf
+      @method('PUT')
+      <div class="row">
+        <div class="col">
+          <div class="mb-3 row">
+            <label class="col-lg-2 col-md-6 col-sm-12 col-form-label">Name:</label>
+            <div class="col-lg-10 col-md-6 col-sm-12">
+              <input name="name" value="{{ $viewData['product']->getName() }}" type="text" class="form-control">
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="mb-3 row">
+            <label class="col-lg-2 col-md-6 col-sm-12 col-form-label">Price:</label>
+            <div class="col-lg-10 col-md-6 col-sm-12">
+              <input name="price" value="{{ $viewData['product']->getPrice() }}" type="number" class="form-control">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="mb-3 row">
+            <label class="col-lg-2 col-md-6 col-sm-12 col-form-label">Image:</label>
+            <div class="col-lg-10 col-md-6 col-sm-12">
+              <input class="form-control" type="file" name="image">
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          &nbsp;
+        </div>
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Description</label>
+        <textarea class="form-control" name="description"
+          rows="3">{{ $viewData['product']->getDescription() }}</textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">Edit</button>
+    </form>
+  </div>
+</div>
+@endsection
---
+++ Chapter21/onlineStore/resources/views/product/show.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section('title', $viewData["title"])
+@section('subtitle', $viewData["subtitle"])
+@section('content')
+<div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="{{ asset('/storage/'.$viewData["product"]->getImage()) }}" class="img-fluid rounded-start">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">
+          {{ $viewData["product"]->getName() }} (${{ $viewData["product"]->getPrice() }})
+        </h5>
+        <p class="card-text">{{ $viewData["product"]->getDescription() }}</p>
+        <p class="card-text"><small class="text-muted">Add to Cart</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
